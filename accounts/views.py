023ef5067b781694764fd414405b5dbcb72c2b29
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2021-08-28 16:30:25 +0630

    fix some bugs

--- accounts/views.py
+++ accounts/views.py
@@ -121,7 +121,8 @@ def register(request):
             #add customer group as default
             gp = Group.objects.get(name='customer')
             user.groups.add(gp)
-
+            #create customer profile for user
+            Customer.objects.create(user=user)
             #login user
             login(request, user)
             return redirect('/')
